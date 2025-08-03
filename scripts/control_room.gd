13ba6713e5be5fceddd9fa991f4667ee5d86eaae
frankba <62514408+frankbits@users.noreply.github.com>
2025-08-03 17:43:11 +0200

    feat: extract character-rotation-code to also rotate subViewport

--- scripts/control_room.gd
+++ scripts/control_room.gd
@@ -1,11 +1,10 @@
 extends Node3D
 
-@export var cameras: Array[SubViewport]
+@export var rooms: Array[Node3D]
 
 func _ready() -> void:
-	print(cameras)
 	for TVindex in len($TVs.get_children()):
 		var TV: MeshInstance3D = $TVs.get_child(TVindex)
 		var mat := TV.mesh.surface_get_material(4)
-		var newTexture: ViewportTexture = cameras[TVindex].get_texture()
+		var newTexture: ViewportTexture = rooms[TVindex].get_node('SubViewport').get_texture()
 		mat.set("albedo_texture", newTexture)
--- scripts/room.gd
+++ scripts/room.gd
@@ -1,13 +1,18 @@
 extends Node3D
 var inside = false
+@onready var camera: Camera3D = $SubViewport/Camera3D
 
 func _input(event: InputEvent) -> void:
 	if event.is_pressed() and event is InputEventKey and event.keycode == KEY_ENTER:
 		rotation_degrees.y += 90
+		rotate_around(camera)
 		if inside:
-			%Character.position = Vector3.ZERO + %Character.position.rotated(Vector3.UP, PI/2)
-			%Character.rotation_degrees.y += 90
+			rotate_around(%Character)
 
 func toggle_inside(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("char"):
 		inside = !inside
+
+func rotate_around(node3d: Node3D, center: Vector3 = Vector3.ZERO) -> void:
+	node3d.position = center + node3d.position.rotated(Vector3.UP, PI/2)
+	node3d.rotation_degrees.y += 90
