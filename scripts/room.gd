75bf5c123bcd675e6cc003599d4f190f90eb9925
Arbee4ever <arbee@arbeeco.de>
2025-08-03 17:25:59 +0200

    fix: rotate char with room

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,5 +1,13 @@
 extends Node3D
+var inside = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_pressed() and event is InputEventKey and event.keycode == KEY_ENTER:
 		rotation_degrees.y += 90
+		if inside:
+			%Character.position = Vector3.ZERO + %Character.position.rotated(Vector3.UP, PI/2)
+			%Character.rotation_degrees.y += 90
+
+func toggle_inside(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("char"):
+		inside = !inside
